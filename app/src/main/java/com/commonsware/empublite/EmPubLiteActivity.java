7b41f66d4a7b9b13e6cfc5c86c25cdb65c89069d
Alfian <genshibeam@gmail.com>
2016-09-09 16:27:14 +0700

    Tutorial #8 - Setting Up An Activity

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,11 +2,37 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
 
 public class EmPubLiteActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+
+        return (super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Intent i = new Intent(this, SimpleContentActivity.class);
+                startActivity(i);
+
+            case R.id.help:
+                i = new Intent(this, SimpleContentActivity.class);
+                startActivity(i);
+        }
+
+        return (super.onOptionsItemSelected(item));
+
     }
 }
